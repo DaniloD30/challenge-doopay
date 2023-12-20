9db5fb91a072255af48d73944190d9bd98e0770f
Danilo Dias <danilolive30@hotmail.com>
2023-12-20 19:53:02 -0300

    feat: color change when done task

--- src/core/components/CardTask/index.tsx
+++ src/core/components/CardTask/index.tsx
@@ -1,5 +1,5 @@
 import { useListContext } from '@/core/hooks/ListContext';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 interface Props {
 	title: string;
@@ -8,9 +8,13 @@ interface Props {
 }
 export default function Card({ title, isPending, indexCard }: Props) {
 	const { handleChangeStatus } = useListContext();
+	const colorText = useMemo(
+		() => (!isPending ? 'text-slate-400 ' : 'text-black'),
+		[isPending],
+	);
 	return (
 		<div className="flex items-center justify-between h-3 border-2 rounded-lg p-7 w-[630px]">
-			<h1 className="text-black text-lg text-ellipsis overflow-hidden ">
+			<h1 className={`text-lg text-ellipsis overflow-hidden ${colorText}`}>
 				{title}
 			</h1>
 			{isPending ? (
